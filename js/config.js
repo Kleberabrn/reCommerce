527fa1f0a65430ab533ffb8ce5b7eee66885ed63
Kleberabrn <51765545+Kleberabrn@users.noreply.github.com>
2026-06-28 01:14:30 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,2 +1,3 @@
-const SUPABASE_URL = 'https://gbokxayrlpucxkkkuafo.supabase.co';
-const SUPABASE_ANON_KEY = 'sb_publishable_DCfa3U3U_A2-weFKugEkCw_wYwsiNqS';
+// js/config.js
+window.SUPABASE_URL = 'https://gbokxayrlpucxkkkuafo.supabase.co';
+window.SUPABASE_ANON_KEY = 'sb_publishable_DCfa3U3U_A2-weFKugEkCw_wYwsiNqS';
